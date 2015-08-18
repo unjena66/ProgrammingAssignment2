8beac9ce609049d17800715e541387c8e3c22011
Jongsoo Han <unjena66@hotmail.com>
2015-08-18 23:41:39 +0900

    1st complete

--- cachematrix.R
+++ cachematrix.R
@@ -2,13 +2,34 @@
 
 ## creates a special "matrix" object that can cache its inverse
 
+## argument 'x' is original matrix to be calculated
 makeCacheMatrix <- function(x = matrix()) {
-        inversematrix <- NULL           # empty storage for inversematrix 
-        # store inversematrix after solve calculation
         
-        reset <- function(){ inversematrix <- NULL }
+        # empty storage for inversematrix 
+        inversematrix <- NULL           
         
+        # store inversematrix after solve calculation or whatever outside do
+        storeInvMtrx <- function(invmtrx){
+                inversematrix <<- invmtrx
+        }
         
+        # get inversematrix for cache
+        getInvMtrx <- function() inversematrix
+        
+        # get original matrix for solve calculation 
+        # original matrix is argument 'x' in this function
+        getOriMtrx <- function() x
+        
+        # renew original matrix (inversematrix needed to be reset when renewing original)
+        # original matrix is argument 'x' in this function
+        renewOriMtrx <- function(newmtrx){
+                inversematrix <<- NULL
+                x <<- newmtrx
+        }
+        
+        # return list of functions to be used outside. name every elements to use readily
+        list(storeInvMtrx = storeInvMtrx, getInvMatrx = getInvMatrx, 
+             getOriMtrx = getOriMtrx, renewOriMtrx )
 }
 
 
@@ -16,8 +37,27 @@ makeCacheMatrix <- function(x = matrix()) {
 ## If the inverse has already been calculated (and the matrix has not changed), 
 ## then the cachesolve should retrieve the inverse from the cache.
 
+## argument 'x' is special matrix object(consisting of original matrix and
+## related functions) made already
+
 cacheSolve <- function(x, ...) {
         
-        inversematrix <- solve(x)
+        # get cached inversematrix
+        inversematrix <- x$getInvMatrx()
+        
+        # if not calculated so that no value for inversematrix,
+        # calculate and assign
+        if(is.null(inversematrix) == TRUE){
+                originalmatrix <- x$getOriMtrx()
+                inversematrix <- solve(originalmatrix)
+                x$storeInvMtrx(inversematrix)
+        
+        # if cacluated already and assigned,
+        # just get the cached inversematrix
+        }else{
+                message ("getting cached data")
+        }
+        
         ## Return a matrix that is the inverse of 'x'
+        return(inversematrix)
 }
